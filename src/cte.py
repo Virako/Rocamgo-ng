783870447f95afd3818e68772bdf8cc21e5d4847
Victor Ramirez <virako.9@gmail.com>
2012-08-13 10:51:37 +0200

    Add headers and substituted numbers for variables.

--- src/cte.py
+++ src/cte.py
@@ -18,36 +18,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from src.cameras import Cameras
-from sys import path
-path.append('/usr/lib/pymodules/python2.7')
-from cv import ShowImage
-from cv import WaitKey
-
-
-def main():
-    # Select camera from computer
-    cam = Cameras()
-    cams_found = cam.check_cameras()
-    camera = cam.show_and_select_camera()
-    
-    while camera: 
-        
-        # Show current camera
-        img = camera.get_frame()
-        ShowImage("Camera", img)
-
-        # Detect goban
-        
-        # Detect stone
-        
-        # Upload to internet
-
-        # FPS
-        key = WaitKey(30)
-        if key == 27: # Esc
-            break
-
-    
-if __name__ == "__main__":
-    main()
+NUM_EDGES = 4
+RELATION_WEIGHT_HEIGHT = 14/15.0
+MAX_CAMERAS = 99
