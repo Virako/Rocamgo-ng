ed0e465ac6cc0e4e38ec311934d97a2cd882fd80
Victor Ramirez <virako.9@gmail.com>
2016-02-20 11:16:52 +0100

    Check install opencv 3 for python3 in travisci

---
+++ tests/main.py
@@ -0,0 +1,11 @@
+import unittest
+
+from rocamgo.cte import BLACK
+
+class TestStringMethods(unittest.TestCase):
+
+  def test_cte(self):
+      self.assertEqual(BLACK, 1)
+
+if __name__ == '__main__':
+    unittest.main()
