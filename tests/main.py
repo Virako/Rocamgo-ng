91534d6522b6978614cf8da982a71c921bf66878
Victor Ramirez <virako.9@gmail.com>
2016-02-20 16:04:07 +0100

    Test opencv

--- tests/main.py
+++ tests/main.py
@@ -7,5 +7,8 @@ class TestStringMethods(unittest.TestCase):
   def test_cte(self):
       self.assertEqual(BLACK, 1)
 
+  def test_import_opencv(self):
+      import cv2
+
 if __name__ == '__main__':
     unittest.main()
